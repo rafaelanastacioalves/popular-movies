4ef020e603990a4f8539fc465e0d225ac6d2e16a
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2016-06-07 18:26:15 -0300

    Add getter and setter for Movie class

--- app/src/main/java/com/example/rafaelanastacioalves/popularmovies/entities/Movie.java
+++ app/src/main/java/com/example/rafaelanastacioalves/popularmovies/entities/Movie.java
@@ -7,9 +7,58 @@ package com.example.rafaelanastacioalves.popularmovies.entities;
 public class Movie {
     private String id;
     private String posterPath;
+    private String originalTitle;
+    private String plotedSynopsis;
+    private String userRating;
+    private String releaseDate;
+
 
     public Movie (String id, String posterPath){
         this.id=id;
         this.posterPath=posterPath;
+    }
+
+    public Movie (String id){
+        this.id=id;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getPlotedSynopsis() {
+        return plotedSynopsis;
+    }
+
+    public void setPlotedSynopsis(String plotedSynopsis) {
+        this.plotedSynopsis = plotedSynopsis;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
     }
 }
