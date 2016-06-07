9e96d09a67fbe763f2e589a9e9d80e33f9aadfd8
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2016-06-07 18:27:19 -0300

    Implements retrieving EXTRA for DetailsActivity

--- app/src/main/java/com/example/rafaelanastacioalves/popularmovies/MovieDetailFragment.java
+++ app/src/main/java/com/example/rafaelanastacioalves/popularmovies/MovieDetailFragment.java
@@ -1,15 +1,27 @@
 package com.example.rafaelanastacioalves.popularmovies;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieDetailFragment extends Fragment {
+
+
+    private static final String TAG_NAME = this.getClass().getSimpleName();
+
+    public static String ORIGINAL_TITLE;
+    public static String MOVIE_POSTER_URL;
+    public static String PLOTED_SYNOPSIS;
+    public static String USER_RATING;
+    public static String RELEASE_DATE;
 
     public MovieDetailFragment() {
     }
@@ -17,6 +29,47 @@ public class MovieDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
+        Log.d(TAG_NAME,"retrieving EXTRAS");
+
+        Intent intent = getActivity().getIntent();
+        if(intent!= null) {
+            if (intent.hasExtra(ORIGINAL_TITLE)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
+            if (intent.hasExtra(MOVIE_POSTER_URL)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
+            if (intent.hasExtra(PLOTED_SYNOPSIS)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
+            if (intent.hasExtra(USER_RATING)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
+            if (intent.hasExtra(RELEASE_DATE)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
+        }
+
+
+
+        return rootView;
     }
+
+
+
+
+
+
+
 }
