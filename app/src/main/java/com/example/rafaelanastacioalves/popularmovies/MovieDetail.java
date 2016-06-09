e41b4404601d704925e4422e946a0a07d2f69fb8
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2016-06-09 20:10:31 -0300

    Setup up to parent behavior and layout for detail acivity

--- app/src/main/java/com/example/rafaelanastacioalves/popularmovies/MovieDetail.java
+++ app/src/main/java/com/example/rafaelanastacioalves/popularmovies/MovieDetail.java
@@ -2,7 +2,6 @@ package com.example.rafaelanastacioalves.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MovieDetail extends AppCompatActivity {
 
@@ -10,8 +9,8 @@ public class MovieDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 
     }
