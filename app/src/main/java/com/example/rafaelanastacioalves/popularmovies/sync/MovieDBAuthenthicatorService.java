250491d197c8a2e0812ea02f14cd96d38f91ca8b
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2016-06-27 20:12:56 -0300

    Implements some of the sync classes

--- app/src/main/java/com/example/rafaelanastacioalves/popularmovies/sync/MovieDBAuthenthicatorService.java
+++ app/src/main/java/com/example/rafaelanastacioalves/popularmovies/sync/MovieDBAuthenthicatorService.java
@@ -1,8 +1,24 @@
 package com.example.rafaelanastacioalves.popularmovies.sync;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by rafael.alves on 27/06/16.
  */
 
-public class MovieDBAuthenthicatorService {
+public class MovieDBAuthenthicatorService extends Service {
+    MovieDBAuthenthicator mAuthenthicator ;
+    @Override
+    public void onCreate() {
+         mAuthenthicator = new MovieDBAuthenthicator(getBaseContext());
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return  mAuthenthicator.getIBinder();
+    }
 }
--- app/src/main/java/com/example/rafaelanastacioalves/popularmovies/sync/MovieDBSyncAdapter.java
+++ app/src/main/java/com/example/rafaelanastacioalves/popularmovies/sync/MovieDBSyncAdapter.java
@@ -1,8 +1,23 @@
 package com.example.rafaelanastacioalves.popularmovies.sync;
 
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
 /**
  * Created by rafael.alves on 27/06/16.
  */
 
-public class MovieDBSyncAdapter {
+public class MovieDBSyncAdapter extends AbstractThreadedSyncAdapter {
+    public MovieDBSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
+    }
 }
