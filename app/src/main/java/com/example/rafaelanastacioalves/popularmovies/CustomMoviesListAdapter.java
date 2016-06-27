decd1a8e304278aaaf16c6c8dc5849d59a1c2f52
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2016-06-27 18:31:15 -0300

    Creates the projection we are gonna need

--- app/src/main/java/com/example/rafaelanastacioalves/popularmovies/CustomMoviesListAdapter.java
+++ app/src/main/java/com/example/rafaelanastacioalves/popularmovies/CustomMoviesListAdapter.java
@@ -1,12 +1,15 @@
 package com.example.rafaelanastacioalves.popularmovies;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ImageView;
 
+import com.example.rafaelanastacioalves.popularmovies.data.MovieContract;
 import com.example.rafaelanastacioalves.popularmovies.entities.Movie;
 import com.squareup.picasso.Picasso;
 
@@ -16,11 +19,21 @@ import java.util.ArrayList;
  * Created by rafael.alves on 08/06/16.
  */
 
-public class CustomMoviesListAdapter extends ArrayAdapter<Movie> {
+public class CustomMoviesListAdapter extends CursorAdapter {
 
-    private final ArrayList<Movie> aMovieList;
+    private final Cursor aMovieList;
 
-    public CustomMoviesListAdapter(Context context, ArrayList<Movie> objects) {
+    public static final String[] PROJETION = new String[] {
+            MovieContract.MovieColumns._ID,
+            MovieContract.MovieColumns.ORIGINAL_TITLE,
+            MovieContract.MovieColumns.PLOTED_SYNOPSIS,
+            MovieContract.MovieColumns.RELEASE_DATE,
+            MovieContract.MovieColumns.POPULARITY,
+            MovieContract.MovieColumns.USER_RATING,
+            MovieContract.MovieColumns.FAVORITE
+    };
+
+    public CustomMoviesListAdapter(Context context, Cursor objects) {
         super(context, 0, objects);
         aMovieList = objects;
     }
@@ -37,6 +50,17 @@ public class CustomMoviesListAdapter extends ArrayAdapter<Movie> {
         return convertView;
     }
 
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+
+    //TODO adapt to cursor...
     public ArrayList<Movie> getItems(){
         return aMovieList;
     }
